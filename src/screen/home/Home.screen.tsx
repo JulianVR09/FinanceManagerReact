7f7e3a38754aadfeeacaddb3e0c6293a71c3aa50
JulianVR09 <julianvillacis.02@gmail.com>
2024-12-09 21:15:19 -0500

    refactoring: the new line in home screen

--- src/screen/home/Home.screen.tsx
+++ src/screen/home/Home.screen.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { useAuth } from '../../contexts/auth.context'
 
 const Home = () => {
+  const { name } = useAuth()
+
   return (
     <View>
       <View>
-
+        <Text>Welcome {name}</Text>
       </View>
       <View>
-        
+
       </View>
       <View>
 
